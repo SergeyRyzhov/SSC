8e2a161735854eb7f1af7bae1685797e2ce564a7
SergeyRyzhov <serega.rf@gmail.com>
2014-03-13 22:37:38 +0400

    Base functionality was implemented.

---
+++ Leaf.DataAccess/Contexts/ConferenceContext.cs
@@ -0,0 +1,45 @@
+﻿using System.Data.Entity;
+using Leaf.DataAccess.Models;
+
+namespace Leaf.DataAccess.Contexts
+{
+    public class ConferenceContext : DbContext
+    {
+        public ConferenceContext()
+            : base("DataConnection")
+        {
+        }
+
+        public DbSet<Conference> Conferences { get; set; }
+
+        public void Clean()
+        {
+            Conferences.RemoveRange(Conferences);
+            Save();
+        }
+
+        public bool Add<T>(T entity) where T : class
+        {
+
+            Entry(entity).State = EntityState.Added;
+            return Save();
+        }
+
+        public bool Update<T>(T entity) where T : class
+        {
+            Entry(entity).State = EntityState.Modified;
+            return Save();
+        }
+
+        public bool Delete<T>(T entity) where T : class
+        {
+            Entry(entity).State = EntityState.Deleted;
+            return Save();
+        }
+
+        public bool Save()
+        {
+            return SaveChanges() > 0;
+        }
+    }
+}
---
+++ Leaf.Web/Controllers/ConferenceController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Leaf.DataAccess.Models;
+using Leaf.DataAccess.Contexts;
+
+namespace Leaf.Web.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private ConferenceContext db = new ConferenceContext();
+
+        // GET: /Conference/
+        public ActionResult Index()
+        {
+            return View(db.Conferences.ToList());
+        }
+
+        // GET: /Conference/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conference conference = db.Conferences.Find(id);
+            if (conference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conference);
+        }
+
+        // GET: /Conference/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Conference/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Code,Name,StartDate,EndDate,Description,ShortDescription,IsPublished")] Conference conference)
+        {
+            if (ModelState.IsValid)
+            {
+                conference.Id = Guid.NewGuid();
+                db.Conferences.Add(conference);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(conference);
+        }
+
+        // GET: /Conference/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conference conference = db.Conferences.Find(id);
+            if (conference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conference);
+        }
+
+        // POST: /Conference/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Code,Name,StartDate,EndDate,Description,ShortDescription,IsPublished")] Conference conference)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(conference).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(conference);
+        }
+
+        // GET: /Conference/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Conference conference = db.Conferences.Find(id);
+            if (conference == null)
+            {
+                return HttpNotFound();
+            }
+            return View(conference);
+        }
+
+        // POST: /Conference/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Conference conference = db.Conferences.Find(id);
+            db.Conferences.Remove(conference);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

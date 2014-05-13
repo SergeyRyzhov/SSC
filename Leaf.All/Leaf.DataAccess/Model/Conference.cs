964d0fe7dfc45f65d79ec65a524e1885abca2a42
SergeyRyzhov <serega.rf@gmail.com>
2014-05-13 23:52:27 +0400

    New solution

---
+++ Leaf.All/Leaf.DataAccess/Model/Conference.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leaf.DataAccess.Model
+{
+    public abstract class Entity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+    }
+
+    public abstract class WebEntity : Entity
+    {
+        public bool Published { get; set; }
+
+        public DateTime PublishDate { get; set; }
+    }
+
+    public class Conference : WebEntity
+    {
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Image> Images { get; set; }
+
+        public List<File> Files { get; set; }
+
+        public List<Report> Reports { get; set; }
+    }
+
+    public class File : WebEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+    }
+
+    public class Image : WebEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+    }
+
+    public class Report : WebEntity
+    {
+        public string Name { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Image> Images { get; set; }
+
+        public List<File> Files { get; set; }
+
+    }
+
+    public abstract class Request : Entity
+    {
+        public bool Approved { get; set; }
+    }
+
+    public class ConferenceRequest : Request
+    {
+        public Guid ConferenceId { get; set; }
+    }
+
+    public class ReportRequest : Request
+    {
+        public Guid ReportId { get; set; }
+    }
+}
